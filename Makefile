d65087d793664ec322a7112072fced176c560dee
dmtrllv <dev.dmtrllv@gmail.com>
2021-06-09 10:08:45 +0200

    added make action to build the final bootable usb and added the BPB to be able to create references inside the bootloader code

--- Makefile
+++ Makefile
@@ -1,17 +1,14 @@
 ARCH = x86
-
 BOOT_BIN = out/$(ARCH)/boot.bin
+BOOT_IMG = out/$(ARCH)/boot.img
+QEMU_ARGS = -drive format=raw,file=$(BOOT_IMG)
 
-QEMU_ARGS = -drive format=raw,file=$(BOOT_BIN)
 
 $(BOOT_BIN): src/arch/$(ARCH)/boot.asm
 	mkdir -p $(@D)
 	nasm -f bin $^ -o $@
 
-all: $(BOOT_BIN)
-	echo $^
-
-run: $(BOOT_BIN)
+run: $(BOOT_IMG)
 ifeq ($(ARCH), x86)
 	qemu-system-i386 $(QEMU_ARGS)
 endif
@@ -21,3 +18,13 @@ endif
 ifeq ($(ARCH), arm)
 	qemu-system-arm $(QEMU_ARGS)
 endif
+
+$(BOOT_IMG): $(BOOT_BIN)
+	test -e $@ || mkfs.fat -F 16 -C $@ 512000
+	# copy the first 3 bytes
+	dd bs=1 if=$(BOOT_BIN) count=3 of=$@ conv=notrunc
+	# skip 91 bytes from out boot.bin because this is the BPB created by the formatter
+	dd bs=1 skip=62 if=$(BOOT_BIN) iflag=skip_bytes of=$@ seek=62 conv=notrunc
+
+clean:
+	rm -rf out/*
--- src/arch/x86/boot.asm
+++ src/arch/x86/boot.asm
@@ -2,6 +2,32 @@
 [org 0x7c00]
 
 boot_start:
+	jmp short boot_resume
+	nop
+
+;-----------------------------------;
+;	Bios Parameter Block (FAT16)	;
+;-----------------------------------; 
+OEMLabel				db "NOVABOOT"		; Disk label
+bytesPerSector			dw 512				; bytes per sector
+sectorsPerCluster		db 1				; Sectors per cluster
+reservedSectors			dw 1				; Reserved sectors for boot record (not included in the root directory / the file system)
+numberOfFats			db 2				; 2 FAT tables to prevent data loss
+rootDirEntries			dw 512				; Number of entries in root dir
+smallNumberOfSectors	dw 0				; Number of small sectors
+mediaDescriptor			db 0xf8				; Medium descriptor byte (0xf8 = fixed disk)
+sectorsPerFat			dw 25				; Sectors per FAT
+sectorsPerTrack			dw 63				; Sectors per track (36/cylinder)
+headsPerCylinder		dw 255				; Number of sides/heads
+hiddenSectors			dd 0				; Number of hidden sectors
+largeSectors			dd 1000				; Number of large sectors
+drive					dw 0x80				; Drive Number
+signature				db 0x29				; Drive signature
+volumeID				dd 0x1234abcd		; Volume ID
+volumeLabel				db "NOVAOS     "	; Volume Label (11 chars)
+fileSystem				db "FAT16   "		; File system type
+
+boot_resume:
 	xor ax, ax
 	mov ds, ax
 	mov es, ax
